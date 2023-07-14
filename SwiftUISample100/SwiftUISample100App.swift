90534f507acdf5b996914b57388daccbd6522bf0
yuki-h29 <yuki.hirano29@gmail.com>
2023-07-14 22:04:23 +0900

    Initial Commit

---
+++ SwiftUISample100/SwiftUISample100App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISample100App.swift
+//  SwiftUISample100
+//
+//  Created by 平野裕貴 on 2023/07/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISample100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
